7bafec8bf4743b32e0c4eb885073b6dc304c6ad4
sj-10ps <sps24723@gmail.com>
2026-01-27 13:17:02 +0530

    added docker and ci

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+#base image(node installed)
+FROM node:22-alpine
+
+#set working directory
+WORKDIR /app
+
+#copy package files
+COPY package*.json ./
+
+#install dependencies
+RUN npm install
+
+#copy rest project
+COPY . .
+
+#build nextjs app
+RUN npm run build
+
+#expose port
+ENV PORT=3000
+EXPOSE 3000
+
+#start app
+CMD [ "npm" ,"run","start" ]
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='bg-blue-700'>
         <div className='flex flex-col gap-6 py-12 items-center px-4'>
-            <h2 className='text-4xl md:text-6xl font-extrabold text-white capitalize'>find the perfect hotel</h2>
+            <h2 className='text-4xl md:text-6xl font-extrabold text-white capitalize'>find the perfect hotel wekjfhwh</h2>
             <p className='text-lg text-white'>Discover the perfect property that suits your needs.</p>
             <PropertySearchBar/>
 
