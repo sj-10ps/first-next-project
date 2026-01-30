c5d9e322148f822c1d6d6df747361065dfacf494
sj-10ps <sps24723@gmail.com>
2026-01-30 12:59:45 +0530

    api fix

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,7 @@
-const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN||null
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN
 //fetch properties
 export const fetchData=async()=>{
+  
   if(apiDomain===null){
     return []
   }
