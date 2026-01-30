b474e6de4db7b926378fdbc8c322476dda68f483
sj-10ps <sps24723@gmail.com>
2026-01-30 14:25:01 +0530

    connected db

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- async headers() {
-    return [
-      {
-        // Apply these headers to all routes under /api
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          // Since it's the same app, you can use your deployed URL 
-          // or a wildcard for testing.
-          { key: "Access-Control-Allow-Origin", value: "*" }, 
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ],
-      },
-    ];
-  },
-
+  /* config options here */
 };
 
 export default nextConfig;
